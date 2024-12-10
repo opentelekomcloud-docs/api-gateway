2cae3f5b56ee853d5efd6f51b5baef87bd3be988
Sebastian Gode <sebastian.gode@telekom.de>
2024-12-10 08:31:41 +0000

    Update tox.ini && conf.py file
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py

--- dev_guide/source/conf.py
+++ dev_guide/source/conf.py
@@ -109,7 +109,7 @@ html_copy_source = False
 # -- Options for PDF output --------------------------------------------------
 latex_documents = [
     ('index',
-     'apig-dev-guide.tex',
+     'apig-dev.tex',
      u'API Gateway (APIG) - Developer Guide',
      u'OpenTelekomCloud', 'manual'),
 ]
